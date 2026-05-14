18ba76ee4a6301f86c9be319d8d4d67010525e22
Y T Sree Purna Akshaya Devi <tejaswiyandamuri06@gmail.com>
2026-05-14 22:35:02 +0530

    Time: 0 ms (100%), Space: 26.5 MB (73.39%) - LeetHub

---
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isGood(vector<int>& nums) {
+        int n=nums.size()-1;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n;i++){
+            if(nums[i]!=i+1)
+                return false;
+        }
+        return nums[n]==n;
+    }
+};
