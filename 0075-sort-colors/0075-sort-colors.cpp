0c81d8b7f2e68315426e24154445a04ff6c5d75e
Y T Sree Purna Akshaya Devi <tejaswiyandamuri06@gmail.com>
2026-05-15 15:48:10 +0530

    Time: 0 ms (100%), Space: 11.6 MB (46.98%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -3,15 +3,8 @@ public:
     void sortColors(vector<int>& nums) {
         int l=0,m=0,h=nums.size()-1;
         while(m<=h){
-            if(nums[m]==0){
-                nums[m]=nums[l];
-                nums[l]=0;
-                l++;m++;
-            }else if(nums[m]==2){
-                nums[m]=nums[h];
-                nums[h]=2;
-                h--;
-            }
+            if(nums[m]==0)swap(nums[m++],nums[l++]);
+            else if(nums[m]==2) swap(nums[m],nums[h--]);
             else
                 m++;
         }   
