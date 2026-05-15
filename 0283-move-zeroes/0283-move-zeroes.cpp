bb9f460823b983e9ae21e9495d0612a4f6ffd6c8
Y T Sree Purna Akshaya Devi <tejaswiyandamuri06@gmail.com>
2026-05-15 12:31:32 +0530

    Time: 0 ms (100%), Space: 23.9 MB (18.4%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int z=0;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]!=0){
+                nums[z++]=nums[i];
+            }
+        }
+        for(int i=z;i<nums.size();i++)
+            nums[i]=0;
+    }
+};
