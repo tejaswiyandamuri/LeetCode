fed78b86029a8f6dd2e22d38127ee8d4ee8c371a
Y T Sree Purna Akshaya Devi <tejaswiyandamuri06@gmail.com>
2026-05-19 13:18:29 +0530

    Time: 0 ms (100%), Space: 54.5 MB (81.53%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.cpp
+++ 2540-minimum-common-value/2540-minimum-common-value.cpp
@@ -2,13 +2,16 @@ class Solution {
 public:
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
         int n1=nums1.size(),n2=nums2.size();
-        unordered_map<int,int> freq;
-        for(auto n1:nums1){
-            freq[n1]++;
-        }
-        for(auto n2:nums2){
-            if(freq[n2]>=1)
-                return n2;
+        int i=0,j=0;
+        while(i<n1&&j<n2){
+            if(nums1[i]==nums2[j])
+                return nums2[j];
+            while(j<n2&&nums2[j]<=nums1[i]){
+                if(nums1[i]==nums2[j])
+                return nums2[j];
+                j++;
+            }
+            i++;
         }
         return -1;
     }
