abad4a5b4d8deb9259b0492089b4447646756e4a
Tejaswi Yandamuri <tejaswiyandamuri06@gmail.com>
2026-06-15 23:09:02 +0530

    Time: 3 ms (24.98%), Space: 28.2 MB (50.79%) - LeetHub

---
+++ 2974-minimum-number-game/2974-minimum-number-game.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> numberGame(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int i=0;
+        while(i<nums.size()-1){
+            swap(nums[i],nums[i+1]);
+            i+=2;
+        }
+        return nums;
+    }
+};
