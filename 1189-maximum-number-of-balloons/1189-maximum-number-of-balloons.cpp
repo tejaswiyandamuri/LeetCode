388faf49beb2e356bd4ea2ccdc14ff1dbb8a92db
Tejaswi Yandamuri <tejaswiyandamuri06@gmail.com>
2026-06-22 09:37:21 +0530

    Time: 3 ms (12.33%), Space: 9 MB (39.61%) - LeetHub

---
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxNumberOfBalloons(string text) {
+        unordered_map<char,int> mp;
+        for(char t:text)
+            mp[t]++;
+        int x=min(min(mp['b'],mp['a']),mp['n']);
+        if(mp['l']==x*2&&mp['o']==x*2)
+            return x;
+        int y=min(mp['l'],mp['o']);
+        // if(y/2!=0){
+        //     y-=1;
+        // }  
+        return min(x,y/2);
+    }
+};
