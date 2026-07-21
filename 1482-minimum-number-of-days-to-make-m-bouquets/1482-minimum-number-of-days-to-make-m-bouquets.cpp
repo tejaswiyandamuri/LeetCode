697ea901b1a53f4445e44929ca8a699a2cd05eb6
Tejaswi Yandamuri <tejaswiyandamuri06@gmail.com>
2026-07-21 21:51:35 +0530

    Time: 16 ms (91.16%), Space: 70.6 MB (48.23%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,12 +1,10 @@
 class Solution {
 public:
-    // Return the number of maximum bouquets that can be made on day mid.
     int getNumOfBouquets(vector<int>& bloomDay, int mid, int k) {
         int numOfBouquets = 0;
 
         int count = 0;
         for (int i = 0; i < bloomDay.size(); i++) {
-            // If the flower is bloomed, add to the set. Else reset the count.
             if (bloomDay[i] <= mid) {
                 count++;
             } else {
