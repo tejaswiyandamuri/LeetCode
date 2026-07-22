e3b141364d7cd5555a2c45d77827bb5bc1f782b5
Tejaswi Yandamuri <tejaswiyandamuri06@gmail.com>
2026-07-22 14:23:33 +0530

    Time: 19 ms (45.03%), Space: 25 MB (68.66%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        int n=strs.size();
+        unordered_map<string,vector<string>> mp;
+        vector<vector<string>> res;
+        for(int i=0;i<strs.size();i++){
+            string cp=strs[i];
+            sort(cp.begin(),cp.end());
+            mp[cp].push_back(strs[i]);
+        }
+        for(auto &x:mp){
+            res.push_back(x.second);
+        }
+        return res;
+    }
+};
