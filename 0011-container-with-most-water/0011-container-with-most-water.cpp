1afc6af981796e7732b12ca692296ec3a2fca827
Tejaswi Yandamuri <tejaswiyandamuri06@gmail.com>
2026-07-24 20:40:09 +0530

    Time: 0 ms (100%), Space: 63 MB (49.65%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int res=INT_MIN,n=height.size();
+        int l=0,r=n-1;
+        while(l<r){
+            res=max(res,min(height[l],height[r])*(r-l));
+            if(height[l]<=height[r])
+                l++;
+            else
+                r--;
+        }
+        return res;
+    }
+};
